1b901e93d29f29017a0e9cda369117378e5fad20
s1lencebtw <Vitalii.Letnianchyn.RI.2023@lpnu.ua>
2023-11-28 20:34:38 +0200

    Add project files.

---
+++ Lab 8.1 string/Lab 8.1 string.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int Count(const std::string s)
+{
+	const char charsToCount[] = { 'B', 'A', 'S', 'I', 'C'};
+
+	int k = 0;
+	for (char c : charsToCount) {
+		size_t pos = 0;
+		while ((pos = s.find(c, pos)) != -1) {
+			pos++;
+			k++;
+		}
+	}
+
+	return k;
+}string Change(string& s)
+{
+	const string stringToReplace = "BASIC";
+	const string replacementString = "DELPHI";
+
+	size_t pos = 0;
+	while ((pos = s.find(stringToReplace, pos)) != -1)
+	{
+		s.replace(pos, stringToReplace.length(), replacementString);
+		pos += replacementString.length();
+	}
+
+	return s;
+}
+
+int main()
+{
+	string str;
+	cout << "Enter string:" << endl;
+	getline(cin, str);
+	cout << "Number of chars BASIC:    " << Count(str) << endl;
+	string dest = Change(str);
+	cout << "Modified string (param) : " << str << endl;
+	cout << "Modified string (result): " << dest << endl;
+	return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab 8.1 string/Lab 8.1 string.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+    TEST_CLASS(UnitTest)
+    {
+    public:
+
+        TEST_METHOD(FindCharTest)
+        {
+            char str[12] = "BASIC";
+            int result = Count(str);
+            Assert::AreEqual(result, 5);
+        }
+    };
+}
